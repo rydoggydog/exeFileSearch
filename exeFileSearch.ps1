f9ecb151ddacd9468e78cfefb263951e59bd2d52
rydoggydog <caufield.ryan@westada.org>
2017-12-23 02:14:54 -0700

    First Commit of Script

---
+++ exeFileSearch.ps1
@@ -0,0 +1,16 @@
+﻿Param(
+    [string]$Extension
+)
+
+$siteCode = Read-Host -Prompt 'Input your site code... The letters no spaces case does not matter.'
+$localPath = 'H:\'
+$ext = 'EXE_Report.csv'
+
+$siteCodeUpper = $siteCode.ToUpper()
+$fileName = "$localPath $(get-date -f yyyy-MM-dd)$ext"
+$folderName = "\\wasd-students\Students\$siteCodeUpper"
+
+
+
+Get-ChildItem -Path $folderName -Include *.exe -Recurse |
+    Select-Object Name,BaseName,FullName,Extension,Directory,LastWriteTime | Export-Csv $fileName -NoTypeInformation
